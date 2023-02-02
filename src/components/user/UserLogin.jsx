1752516b3868fa667a2a41c9b7e4c37849378c58
Taresh <tareshbhasin04@gmail.com>
2023-02-02 13:23:17 +0530

    updated 1st branch userlogin comment

--- src/components/user/UserLogin.jsx
+++ src/components/user/UserLogin.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Hoc from '../admin/Hoc'
+// import { useEffect } from 'react';
+// import { useState } from 'react'
 
-const UserLogin = () => {
-  return (
-    <div>UserLogin</div>
+const UserLogin = ({ data }) => {
+    // const [userList,setUserList] = useState([]);
+    // const [term, setTerm] = useState('')
+    // useEffect(()=>{
+    //     const fetchUsers = async ()=> {
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    //         const jsonRes = await res.json();
+    //         setUserList(jsonRes);
+    //         console.log(jsonRes);
+    //     }
+    //     fetchUsers()
+    // },[]) 
+    // const filteredUser = userList.filter(({name})=>(name.indexOf(term) >=0)).map((user)=>(<div key={user.id}><strong>{user.name}</strong></div>))
+   const filteredData = data.map((user)=>(<div key={user.id}><strong>{user.name}</strong></div>))
+    return (
+
+    <>
+        <div>UserLogin</div>
+        {/* <input type='text' value={term} onChange={(e)=>setTerm(e.target.value)}></input> */}
+         <p/>
+        {/* {userList.map((user)=>(
+            <div key={user.id}><strong>{user.name}</strong></div>
+        ))} */}
+        {/* {userList.filter(({name})=>(name.indexOf(term) >=0)).map((user)=>(<div key={user.id}><strong>{user.name}</strong></div>))} */}
+        {/* {filteredUser} */}
+        {filteredData}
+        <br/>
+    </>
   )
 }
-
-export default UserLogin
+// this is commented line.
+const SearchUsers = Hoc(UserLogin, 'users')
+export default SearchUsers
